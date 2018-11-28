ee1112f49aa1df4acc7e9895bb970121ef03062b
danialfaisal <dfaisal@unomaha.edu>
2018-11-27 18:31:35 -0600

    First commit
    Divided the program into different modules

--- activity8.py
+++ activity8.py
@@ -1,2 +1,48 @@
-# Activity 8
-# Erase the current code in the file and create the code to complete the requirements as stated in the assignment.
+import activity8_db
+from activity8_business import CustomerInfo
+
+
+def display_message():
+    print("Customer Viewer")
+    print()
+
+
+def viewer():
+    customers = activity8_db.get_customer(CustomerInfo)
+    customerid = int(input("Enter customer ID: "))
+    print()
+    if customerid >=101 and customerid <=600:
+        customer = customers[customerid-100]
+        print(customer.fname, customer.lname)
+        if customer.cname == '':
+            pass
+        else:
+            print(customer.cname)
+        print(customer.address)
+        print(customer.city, ",", customer.state, customer.zipcode)
+        print()
+    else:
+        print("No customer with that ID")
+        print()
+
+
+def main2():
+    question = input("Continue (y/n)")
+    if question == "n" or question == "N":
+        print("Bye!")
+    elif question == "y" or question == "Y":
+        print()
+        viewer()
+        main2()
+    else:
+        print("Invalid Input-Enter either y or n")
+        main2()
+
+
+def main():
+    display_message()
+    viewer()
+    main2()
+
+if __name__ == '__main__':
+    main()
---
+++ activity8_business.py
@@ -0,0 +1,10 @@
+class CustomerInfo:
+    def __init__(self, custid=0, fname='', lname='', cname='', address='', city='', state='', zipcode=''):
+        self.custid = custid
+        self.fname = fname
+        self.lname = lname
+        self.cname = cname
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
